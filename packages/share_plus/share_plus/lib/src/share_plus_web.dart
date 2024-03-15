9ba1a7d04283ea4c25699ee7983464fb6fa69fd8
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-03-15 23:26:01 +0900

    feat(share_plus)!: Migrate to package:web

--- packages/share_plus/share_plus/lib/src/share_plus_web.dart
+++ packages/share_plus/share_plus/lib/src/share_plus_web.dart
@@ -1,12 +1,16 @@
-import 'dart:html' as html;
+import 'dart:developer' as developer;
+import 'dart:js_interop';
 import 'dart:typed_data';
+import 'dart:ui';
 
-import 'package:flutter/widgets.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:meta/meta.dart';
 import 'package:mime/mime.dart' show lookupMimeType;
 import 'package:share_plus_platform_interface/share_plus_platform_interface.dart';
 import 'package:url_launcher_platform_interface/url_launcher_platform_interface.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:web/web.dart' as web
+    show DOMException, File, FilePropertyBag, Navigator, window;
 
 /// The web implementation of [SharePlatform].
 class SharePlusWebPlugin extends SharePlatform {
@@ -17,50 +21,98 @@ class SharePlusWebPlugin extends SharePlatform {
     SharePlatform.instance = SharePlusWebPlugin(UrlLauncherPlugin());
   }
 
-  final html.Navigator _navigator;
+  final web.Navigator _navigator;
 
   /// A constructor that allows tests to override the window object used by the plugin.
   SharePlusWebPlugin(
     this.urlLauncher, {
-    @visibleForTesting html.Navigator? debugNavigator,
-  }) : _navigator = debugNavigator ?? html.window.navigator;
+    @visibleForTesting web.Navigator? debugNavigator,
+  }) : _navigator = debugNavigator ?? web.window.navigator;
 
-  /// Share text
+  @override
+  Future<void> shareUri(
+    Uri uri, {
+    Rect? sharePositionOrigin,
+  }) async {
+    final data = ShareData.url(
+      url: uri.toString(),
+    );
+
+    if (!_canShare(data)) {
+      return;
+    }
+
+    try {
+      await _navigator.share(data).toDart;
+    } on web.DOMException catch (e) {
+      // Ignore DOMException
+      developer.log(
+        'Failed to share uri',
+        error: '${e.name}: ${e.message}',
+      );
+    }
+  }
+
   @override
   Future<void> share(
     String text, {
     String? subject,
     Rect? sharePositionOrigin,
   }) async {
-    try {
-      await _navigator.share({'title': subject, 'text': text});
-    } on NoSuchMethodError catch (_) {
-      //Navigator is not available or the webPage is not served on https
-      final queryParameters = {
-        if (subject != null) 'subject': subject,
-        'body': text,
-      };
-
-      // see https://github.com/dart-lang/sdk/issues/43838#issuecomment-823551891
-      final uri = Uri(
-        scheme: 'mailto',
-        query: queryParameters.entries
-            .map((e) =>
-                '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
-            .join('&'),
-      );
-
-      final launchResult = await urlLauncher.launchUrl(
-        uri.toString(),
-        const LaunchOptions(),
-      );
-      if (!launchResult) {
-        throw Exception('Failed to launch $uri');
-      }
-    }
+    await shareWithResult(
+      text,
+      subject: subject,
+      sharePositionOrigin: sharePositionOrigin,
+    );
   }
 
-  /// Share files
+  @override
+  Future<ShareResult> shareWithResult(
+    String text, {
+    String? subject,
+    Rect? sharePositionOrigin,
+  }) async {
+    final ShareData data;
+    if (subject != null && subject.isNotEmpty) {
+      data = ShareData.textWithSubject(
+        title: subject,
+        text: text,
+      );
+    } else {
+      data = ShareData.text(
+        text: text,
+      );
+    }
+
+    if (!_canShare(data)) {
+      // share function is not available, try to handle mailto scheme via url_launcher
+      await _handleMailToScheme(
+        text: text,
+        subject: subject,
+      );
+      return _resultUnavailable;
+    }
+
+    try {
+      await _navigator.share(data).toDart;
+
+      // actions is success, but can't get the action name
+      return _resultUnavailable;
+    } on web.DOMException catch (e) {
+      if (e.name case 'AbortError') {
+        return _resultDismissed;
+      }
+
+      // share action is not executed, try to handle mailto scheme via url_launcher
+      await _handleMailToScheme(
+        text: text,
+        subject: subject,
+      );
+    }
+
+    return _resultUnavailable;
+  }
+
   @override
   Future<void> shareFiles(
     List<String> paths, {
@@ -68,12 +120,29 @@ class SharePlusWebPlugin extends SharePlatform {
     String? subject,
     String? text,
     Rect? sharePositionOrigin,
-  }) {
+  }) async {
+    await shareFilesWithResult(
+      paths,
+      mimeTypes: mimeTypes,
+      subject: subject,
+      text: text,
+      sharePositionOrigin: sharePositionOrigin,
+    );
+  }
+
+  @override
+  Future<ShareResult> shareFilesWithResult(
+    List<String> paths, {
+    List<String>? mimeTypes,
+    String? subject,
+    String? text,
+    Rect? sharePositionOrigin,
+  }) async {
     final files = <XFile>[];
     for (var i = 0; i < paths.length; i++) {
       files.add(XFile(paths[i], mimeType: mimeTypes?[i]));
     }
-    return shareXFiles(
+    return await shareXFiles(
       files,
       subject: subject,
       text: text,
@@ -85,9 +154,7 @@ class SharePlusWebPlugin extends SharePlatform {
   ///
   /// Remarks for the web implementation:
   /// This uses the [Web Share API](https://web.dev/web-share/) if it's
-  /// available. Otherwise, uncaught Errors will be thrown.
-  /// See [Can I Use - Web Share API](https://caniuse.com/web-share) to
-  /// understand which browsers are supported. This builds on the
+  /// available. This builds on the
   /// [`cross_file`](https://pub.dev/packages/cross_file) package.
   @override
   Future<ShareResult> shareXFiles(
@@ -96,29 +163,107 @@ class SharePlusWebPlugin extends SharePlatform {
     String? text,
     Rect? sharePositionOrigin,
   }) async {
-    // See https://developer.mozilla.org/en-US/docs/Web/API/Navigator/share
-
-    final webFiles = <html.File>[];
+    final webFiles = <web.File>[];
     for (final xFile in files) {
       webFiles.add(await _fromXFile(xFile));
     }
-    await _navigator.share({
-      if (subject?.isNotEmpty ?? false) 'title': subject,
-      if (text?.isNotEmpty ?? false) 'text': text,
-      if (webFiles.isNotEmpty) 'files': webFiles,
-    });
+
+    final ShareData data;
+    if (text != null && text.isNotEmpty) {
+      if (subject != null && subject.isNotEmpty) {
+        data = ShareData.filesWithTextAndSubject(
+          files: webFiles.toJS,
+          text: text,
+          title: subject,
+        );
+      } else {
+        data = ShareData.filesWithText(
+          files: webFiles.toJS,
+          text: text,
+        );
+      }
+    } else if (subject != null && subject.isNotEmpty) {
+      data = ShareData.filesWithTextAndSubject(
+        files: webFiles.toJS,
+        title: subject,
+        text: '',
+      );
+    } else {
+      data = ShareData.files(
+        files: webFiles.toJS,
+      );
+    }
+
+    if (!_canShare(data)) {
+      return _resultUnavailable;
+    }
+
+    try {
+      await _navigator.share(data).toDart;
+
+      // actions is success, but can't get the action name
+      return _resultUnavailable;
+    } on web.DOMException catch (e) {
+      developer.log(
+        'Failed to share files',
+        error: '${e.name}: ${e.message}',
+      );
+
+      if (e.name case 'AbortError') {
+        return _resultDismissed;
+      }
+    }
 
     return _resultUnavailable;
   }
 
-  static Future<html.File> _fromXFile(XFile file) async {
+  bool _canShare(ShareData data) {
+    try {
+      return _navigator.canShare(data);
+    } on web.DOMException catch (e) {
+      developer.log(
+        'Share API is not supported in this User Agent.',
+        error: '${e.name}: ${e.message}',
+      );
+
+      return false;
+    }
+  }
+
+  Future<void> _handleMailToScheme({
+    required String text,
+    required String? subject,
+  }) async {
+    final queryParameters = {
+      if (subject != null) 'subject': subject,
+      'body': text,
+    };
+
+    // see https://github.com/dart-lang/sdk/issues/43838#issuecomment-823551891
+    final uri = Uri(
+      scheme: 'mailto',
+      query: queryParameters.entries
+          .map((e) =>
+              '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+          .join('&'),
+    );
+
+    final launchResult = await urlLauncher.launchUrl(
+      uri.toString(),
+      const LaunchOptions(),
+    );
+    if (!launchResult) {
+      throw Exception('Failed to launch $uri');
+    }
+  }
+
+  static Future<web.File> _fromXFile(XFile file) async {
     final bytes = await file.readAsBytes();
-    return html.File(
-      [ByteData.sublistView(bytes)],
+    return web.File(
+      [bytes.buffer.toJS].toJS,
       file.name,
-      {
-        'type': file.mimeType ?? _mimeTypeForPath(file, bytes),
-      },
+      web.FilePropertyBag()
+        ..type = file.mimeType ?? _mimeTypeForPath(file, bytes),
     );
   }
 
@@ -128,7 +273,50 @@ class SharePlusWebPlugin extends SharePlatform {
   }
 }
 
+const _resultDismissed = ShareResult(
+  '',
+  ShareResultStatus.dismissed,
+);
+
 const _resultUnavailable = ShareResult(
   'dev.fluttercommunity.plus/share/unavailable',
   ShareResultStatus.unavailable,
 );
+
+extension on web.Navigator {
+  /// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/canShare
+  external bool canShare(ShareData data);
+
+  /// https://developer.mozilla.org/en-US/docs/Web/API/Navigator/share
+  external JSPromise share(ShareData data);
+}
+
+extension type ShareData._(JSObject _) implements JSObject {
+  external factory ShareData.text({
+    String text,
+  });
+
+  external factory ShareData.textWithSubject({
+    String text,
+    String title,
+  });
+
+  external factory ShareData.files({
+    JSArray<web.File> files,
+  });
+
+  external factory ShareData.filesWithText({
+    JSArray<web.File> files,
+    String text,
+  });
+
+  external factory ShareData.filesWithTextAndSubject({
+    JSArray<web.File> files,
+    String text,
+    String title,
+  });
+
+  external factory ShareData.url({
+    String url,
+  });
+}
