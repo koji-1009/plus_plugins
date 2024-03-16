a5c69d373b830b2643c7ff8a850be51070361a27
Koji Wakamiya <koji.wakamiya@gmail.com>
2024-03-16 18:17:25 +0900

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "fde802b6ed3a60484f6fb2b74131b51a",
-"index.html": "343dce21342fb9c74c30d6c6b166b0d7",
-"/": "343dce21342fb9c74c30d6c6b166b0d7",
-"main.dart.js": "eca19b0200d5eeb36f807264e384e539",
+"index.html": "938ff19c967f6d8b3072036d6efae04c",
+"/": "938ff19c967f6d8b3072036d6efae04c",
+"main.dart.js": "0425894ea5a38be11cb4907cb93e62b7",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
