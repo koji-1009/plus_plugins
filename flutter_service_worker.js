80088acfd285e0cbf6b9761c43d0d7bd9e4bdcf2
koji-1009 <koji-1009@users.noreply.github.com>
2024-03-18 10:42:14 +0000

    deploy: e083d09c45350aa313c7ff8cb174370c355721a5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "a75afbe5a1e5251a98dcb37b671c5fd8",
-"/": "a75afbe5a1e5251a98dcb37b671c5fd8",
+const RESOURCES = {"index.html": "af676a5e046b0e27612da578acc0afdd",
+"/": "af676a5e046b0e27612da578acc0afdd",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
